575b550cd132376f9bb6360e35c7cbe0faab00f7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-08-28 17:34:35 +0300

    api loading done, displaye to the ui, displaye details done

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ const loadMeals = (search) => {
 }
 
 const displayMeals = (meals) => {
+    const mealHolder = document.getElementById('meal-holder');
+    mealHolder.innerHTML = '';
+
     meals.forEach(meal => {
         console.log(meal);
-        const mealHolder = document.getElementById('meal-holder');
         const mealCard = document.createElement('div')
         mealCard.classList.add('col-4')
         mealCard.innerHTML =
-            ` <div class="card h-100 ">
+            ` <div class="card h-100 " onclick="loadDetails(${meal.idMeal})">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body text-center">
                         <p class="card-title fs-5 fw-bolder">${meal.strMeal}</p>
@@ -23,7 +25,7 @@ const displayMeals = (meals) => {
                         <p class="card-text fw-regular ">Country: ${meal.strArea}</p>
                         <div class="d-grid gap-2 ">
                             <button class="btn btn-primary btn-sm fw-semibold btn-select-player"
-                                type="button" style="--bs-btn-padding-y: .6rem;">Order now</button>
+                                type="button" style="--bs-btn-padding-y: .6rem;">Add to cart</button>
                         </div>
                     </div>
                 </div>
@@ -32,8 +34,38 @@ const displayMeals = (meals) => {
     });
 }
 
+const mealSearch = () => {
+    const searchInputElem = document.getElementById('meal-input');
+    console.log(searchInputElem);
+    const inputValue = searchInputElem.value;
+    loadMeals(inputValue);
+    searchInputElem.value = '';
+}
 
-const mealSearch = ()
+const loadDetails = (id) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
 
-loadMeals('chicken');
+const displayMealDetails = (details) => {
+    const detailsHolder = document.getElementById('information-holder');
+    detailsHolder.innerHTML = `
+    <div class="card ">
+        <img src="${details.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+            <p class="card-title fs-5 fw-bolder">${details.strMeal}</p>
+            <p class="card-text fw-semibold ">Catagory: ${details.strCategory}</p>
+            <p class="card-text fw-regular ">Country: ${details.strArea}</p>
+            <div class="d-grid gap-2 ">
+                <button class="btn btn-primary btn-sm fw-semibold btn-select-player" type="button"
+                    style="--bs-btn-padding-y: .6rem;">Order now</button>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+loadMeals('');
 
